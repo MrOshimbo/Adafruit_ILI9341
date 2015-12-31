d36815348ae97e5e17932364d29c62c4115f5ff6
MrOshimbo <mroshimbo@gmail.com>
2015-12-30 20:36:08 -0500

    Update Adafruit_ILI9341_mod.cpp

--- firmware/Adafruit_ILI9341_mod.cpp
+++ firmware/Adafruit_ILI9341_mod.cpp
@@ -14,7 +14,7 @@ Written by Limor Fried/Ladyada for Adafruit Industries.
 MIT license, all text above must be included in any redistribution
 ****************************************************/
 
-#include "Adafruit_ILI9341.h"
+#include "Adafruit_ILI9341_mod.h"
 #include <limits.h>
 
 #include "application.h"
